e3b29239fa9bf1046c1510fdeb70b25cd11d5438
Vinay Kumar Kotegowder <vinaykumar.kotegowder@arm.com>
2018-12-19 11:08:25 +0530

    Updated python code to get the number of occurances of each command per user.

--- predict_next_command.py
+++ predict_next_command.py
@@ -9,9 +9,16 @@ users = glob("/home/*")
 for user in users:
     try:
         with open(user+"/.bash_history", 'r') as user_bash_history:
-            user_bash_details = []
+            user_bash_details = {}
             for command in user_bash_history:
-                user_bash_details.append(command)
+		command_present = False
+		for key in user_bash_details:
+			if(command.rstrip() == key):
+				user_bash_details[command.rstrip()] += 1
+				command_present = True
+				break;
+		if(command_present == False):
+			user_bash_details.update({command.rstrip():1})
             database.update({user:user_bash_details})
     except IOError:
         print("No .bash_history found for user " + user)
